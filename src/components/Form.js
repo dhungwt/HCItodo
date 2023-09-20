d04deefa4b13638ed4f7118c2c1e7b5ca0ff21ef
dhungwt <112122672+dhungwt@users.noreply.github.com>
2023-09-20 13:00:47 -0400

    changed structure in form.js to include a duedate, date does not appear when selected

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,37 +1,48 @@
 import React, { useState } from "react";
 
 function Form(props) {
-    const [name, setName] = useState("");
-    const [dueDate, setDueDate] = useState('')
-//   const [task, setTask] = { name: "", dueDate: "" };
+  // const [name, setName] = useState("");
+  // const [dueDate, setDueDate] = useState('')
+  const [task, setTask] = useState({ name: "", dueDate: "" });
 
   function handleChange(e) {
-    setName(e.target.value);
-    // setTask()
+    const { name, value } = e.target;
+    setTask((prevTask) => ({ //updating the previous state of the task with the new info that was input
+      ...prevTask,
+      [name]: value,
+    }));
   }
 
   function handleSubmit(e) {
-    e.preventDefault();
-    props.addTask(name);
-    const newTask = { name: name, dueDate: dueDate };
+    e.preventDefault(); //prevents refreshing the page every time the form is edited
+    props.addTask(task); //pushes the new task on the tasklist array
+    setTask({ name: "", dueDate: "" }); //resets the form
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <div class="mb-3">
-      
-      <input
-        type="text"
-        id="floatingTextarea"
-        class="form-control form-control-lg" 
-        placeholder="What do you need to get done?"
-        name="text"
-        autoComplete="off"
-        value = {name}
-        onChange={handleChange}
-
-      />
-</div>
+        <input
+          type="text"
+          id="name"
+          class="form-control form-control-lg"
+          placeholder="What do you need to get done?"
+          name="text"
+          autoComplete="off"
+          value={task.name}
+          onChange={handleChange}
+        />
+        <input
+          type="date"
+          id="dueDate"
+          class="form-control form-control-lg"
+          placeholder="When do you need to finish it by?"
+          name="text"
+          autoComplete="off"
+          value={task.dueDate}
+          onChange={handleChange}
+        />
+      </div>
       <button type="submit " className="btn btn__primary btn__lg rounded-pill">
         Add
       </button>

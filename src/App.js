7f5fd65213f5ea81ced05821a3bc5436c5bfd40d
dhungwt <112122672+dhungwt@users.noreply.github.com>
2023-09-25 20:31:14 -0400

    react toast added after bootstrap toast would not work

--- src/App.js
+++ src/App.js
@@ -2,53 +2,63 @@ import "./App.css";
 import Form from "./components/Form";
 import Todo from "./components/Todo";
 import { nanoid } from "nanoid";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import React, { useState } from "react";
 
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
- 
-    function addTask(name, dueDate, category) {
-      const newTask = { id: `todo-${nanoid()}`, name, dueDate, category, completed: false };
-      setTasks([...tasks, newTask]);
-      console.log("INSIDE ADDTASKS: " + tasks)
-    }
 
-    function deleteTask(id) {
-      //filter out all tasks that do not match the id
-      const remainingTasks = tasks.filter((task) => id !== task.id);
-      //save a new version of tasks w/o the task that was deleted
-      setTasks(remainingTasks);
-    }
-    
-    function editTask(id, newName, newDate) {
-      const editedTaskList = tasks.map((task) => {
-        // if this task has the same ID as the edited task
-        if (id === task.id) {
-          //
-          return { ...task, name: newName, dueDate: newDate };
+  function addTask(name, dueDate, category) {
+    const newTask = {
+      id: `todo-${nanoid()}`, //assigns unique ids
+      name,
+      dueDate,
+      category,
+      completed: false,
+    };
+    setTasks([...tasks, newTask]);
+    console.log("INSIDE ADDTASKS: " + tasks);
+  }
+
+  function deleteTask(id) {
+    //filter out all tasks that do not match the id
+    const remainingTasks = tasks.filter((task) => id !== task.id);
+    //save a new version of tasks w/o the task that was deleted
+    setTasks(remainingTasks);
+  }
+
+  function editTask(id, newName, newDate) {
+    const editedTaskList = tasks.map((task) => {
+      // if this task has the same ID as the edited task
+      if (id === task.id) {
+        //
+        return { ...task, name: newName, dueDate: newDate };
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
+
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map((task) => {
+      // if this task has the same ID as the edited task
+      if (id === task.id) {
+        const updatedTask = { ...task, completed: !task.completed };
+
+        if (updatedTask.completed) { //triggers a toast notification when a checkbox is checked
+          toast.success("Task completed! Keep up the good work!", { position: "top-right" });
         }
-        return task;
-      });
-      setTasks(editedTaskList);
-    }
-    
-    
-    function toggleTaskCompleted(id) {
-      const updatedTasks = tasks.map((task) => {
-        // if this task has the same ID as the edited task
-        if (id === task.id) {
-          // use object spread to make a new object
-          // whose `completed` prop has been inverted
-          return { ...task, completed: !task.completed };
-        }
-        //if ids dont match, return original obj
-        return task;
-      });
-      //update our tasks w/ updated tasks
-      setTasks(updatedTasks);
-    }
-    
-    
+        // use object spread to make a new object
+        // whose `completed` prop has been inverted
+        return updatedTask;
+      }
+      //if ids dont match, return original obj
+      return task;
+    });
+    //update our tasks w/ updated tasks
+    setTasks(updatedTasks);
+  }
 
   const taskList = tasks?.map((task) => (
     <Todo
@@ -69,7 +79,9 @@ function App(props) {
       <h1>Diana Hung's To Do List Assignment</h1>
       <Form addTask={addTask} />
       <div className="filters btn-group stack-exception"></div>
-      <h2 id="list-heading"><u>TASK LIST:</u></h2>
+      <h2 id="list-heading">
+        <u>TASK LIST:</u>
+      </h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
